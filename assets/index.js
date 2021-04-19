817637df01e8262b83176114a21657d469506339
soundwanders <soundwanders@gmail.com>
2021-04-19 12:21:56 -0400

    card images pop out to modals on click. css style additions

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,37 @@
-// starmap javascript
+// jquery
+$(document).ready(function() {
 
-const bootstrap = require('bootstrap');
+  // make each card's nebula image a modal on click, use escape key or click again to remove modal, returning card to normal state
+  $('img[data-enlargeable]').addClass('img-enlargeable').click(function () {
+    let src = $(this).attr('src');
+    let modal;
+
+    function removeModal () {
+      modal.remove ();
+      $('body').off('keyup.modal-close');
+    }
+    // add modal CSS
+    modal = $('<div>').css ({
+      background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+      backgroundSize: 'contain',
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      zIndex: '10000',
+      top: '0',
+      left: '0',
+      cursor: 'zoom-out'
+    })
+    .click(function () {
+      removeModal();
+    })
+    .appendTo('body');
+
+    // handling ESC key
+    $('body').on('keyup.modal-close', function (e) {
+      if (e.key === 'Escape') {
+        removeModal();
+      }
+    })
+  });
+});
